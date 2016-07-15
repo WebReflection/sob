1c1633e625ce215047e23293c6315b188ae35840
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-07-15 18:32:02 +0100

    first commit

--- build/sob.node.js
+++ build/sob.node.js
@@ -1,5 +1,5 @@
 /*!
-Copyright (C) 2015 by WebReflection
+Copyright (C) 2016 by Andrea Giammarchi @WebReflection
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,4 +20,114 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-module.exports = {};
+//! (C) Andrea Giammarchi @WebReflection - MIT Style License
+var
+  requestAnimationFrame = global.requestAnimationFrame ||
+                          global.webkitRequestAnimationFrame ||
+                          global.mozRequestAnimationFrame ||
+                          function (fn) {
+                            setTimeout(fn, 16);
+                          },
+  requestIdleCallback = global.requestIdleCallback,
+  NO_IDLE = !requestIdleCallback,
+  performance = global.performance || {now: Date.now},
+  now = (performance.now || performance.webkitNow || function () {
+    return (new Date()).getTime();
+  }),
+  create = function create(callback) {
+    for (var
+      queue = this,
+      args = [],
+      info = {
+        id: {},
+        fn: callback,
+        ar: args
+      },
+      i = 1; i < arguments.length; i++
+    ) args[i - 1] = arguments[i];
+    return infoId(queue, info) || (queue.push(info), info.id);
+  },
+  compareValue = function compareValue(value, i) {
+    return value === this[i];
+  },
+  exec = function exec(info) {
+    info.fn.apply(null, info.ar);
+  },
+  findIndex = function findIndex(queue, id) {
+    var i = queue.length;
+    while (i-- && queue[i].id !== id);
+    return i;
+  },
+  infoId = function infoId(queue, info) {
+    for (var i = 0, tmp; i < queue.length; i++) {
+      tmp = queue[i];
+      if (
+        tmp.fn === info.fn &&
+        tmp.ar.length === info.ar.length &&
+        tmp.ar.every(compareValue, info.ar)
+      ) return tmp.id;
+    }
+    return null;
+  },
+  drop = function drop(queue, id) {
+    var
+      i = findIndex(queue, id),
+      found = -1 < i
+    ;
+    if (found) queue.splice(i, 1);
+    return found;
+  },
+  previousLength = 0,
+  qframe = [],
+  qidle = [],
+  next = {
+    debug: false,
+    isOverloaded: false,
+    minFPS: 60,
+    clear: function clear(id) {
+      if (!drop(qframe, id)) drop(qidle, id);
+    },
+    frame: function frame() {
+      return create.apply(qframe, arguments);
+    },
+    idle: function idle() {
+      return create.apply(qidle, arguments);
+    }
+  }
+;
+
+(function animationLoop() {
+  var
+    t = now.call(performance),
+    fps = 1000 / next.minFPS,
+    overTime = false,
+    info
+  ;
+  while (qframe.length) {
+    exec(qframe.shift());
+    overTime = (now.call(performance) - t) >= fps;
+    if (overTime) break;
+  }
+  if (NO_IDLE && !overTime && qidle.length) {
+    exec(qidle.shift());
+  }
+  next.isOverloaded = qframe.length > previousLength;
+  previousLength = qframe.length;
+  if (next.debug && next.isOverloaded)
+    console.warn('overloaded frame');
+  requestAnimationFrame(animationLoop);
+}());
+
+if (!NO_IDLE) (function idleLoop() {
+  var
+    t = now.call(performance),
+    fps = 1000 / next.minFPS,
+    info
+  ;
+  while (qidle.length) {
+    exec(qidle.shift());
+    if ((now.call(performance) - t) >= fps) break;
+  }
+  requestIdleCallback(idleLoop);
+}());
+module.exports = next;
--- test/sob.js
+++ test/sob.js
@@ -1,13 +1,329 @@
 //remove:
-var main = require('../build/sob.node.js');
+var next = require('../build/sob.node.js');
 //:remove
 
 wru.test([
   {
-    name: "main",
+    name: "it's an obejct",
     test: function () {
-      wru.assert(typeof main == "object");
-      // wru.assert(0);
+      wru.assert(typeof sob == 'object');
+    }
+  }, {
+    name: 'requestAnimationFrame',
+    test: function () {
+      sob.frame(wru.async(function () {
+        wru.assert('requestAnimationFrame happened', true);
+      }));
+    }
+  }, {
+    name: 'requestIdleCallback',
+    test: function () {
+      sob.frame(wru.async(function () {
+        wru.assert('requestIdleCallback happened', true);
+      }));
+    }
+  }, {
+    name: 'clear',
+    test: function () {
+      var
+        animHappened = false,
+        idleHappened = false,
+        animId = sob.frame(function () {
+          animHappened = true;
+        }),
+        idleId = sob.idle(function () {
+          idleHappened = true;
+        })
+      ;
+      sob.clear(animId);
+      sob.clear(idleId);
+      setTimeout(wru.async(function () {
+        wru.assert(!animHappened && !idleHappened);
+      }), 300);
+    }
+  }, {
+    name: 'clear inverted',
+    test: function () {
+      var
+        animHappened = false,
+        idleHappened = false,
+        animId = sob.frame(function () {
+          animHappened = true;
+        }),
+        idleId = sob.idle(function () {
+          idleHappened = true;
+        })
+      ;
+      sob.clear(idleId);
+      sob.clear(animId);
+      setTimeout(wru.async(function () {
+        wru.assert(!animHappened && !idleHappened);
+      }), 300);
+    }
+  }, {
+    name: 'no duplicated rAF',
+    test: function () {
+      var
+        executed = [],
+        first = function () {
+          executed.push(first);
+        },
+        second = function () {
+          executed.push(second);
+        }
+      ;
+      sob.frame(first);
+      sob.frame(first);
+      sob.frame(second);
+      sob.frame(first);
+      sob.frame(second);
+      sob.frame(second);
+      sob.frame(first);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          executed.length === 2 &&
+          executed[0] === first &&
+          executed[1] === second
+        );
+      }), 300);
+    }
+  }, {
+    name: 'no duplicated rIC',
+    test: function () {
+      var
+        executed = [],
+        first = function () {
+          executed.push(first);
+        },
+        second = function () {
+          executed.push(second);
+        }
+      ;
+      sob.idle(first);
+      sob.idle(first);
+      sob.idle(second);
+      sob.idle(first);
+      sob.idle(second);
+      sob.idle(second);
+      sob.idle(first);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          executed.length === 2 &&
+          executed[0] === first &&
+          executed[1] === second
+        );
+      }), 300);
+    }
+  }, {
+    name: 'passed arguments rAF',
+    test: function () {
+      var
+        executed = [],
+        first = function () {
+          executed.push(arguments);
+        },
+        second = function () {
+          executed.push(arguments);
+        }
+      ;
+      sob.frame(first, 1, 2, 3);
+      sob.frame(second, 4, 5, 6);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          [].join.call(executed[0]) === '1,2,3' &&
+          [].join.call(executed[1]) === '4,5,6'
+        );
+      }), 300);
+    }
+  }, {
+    name: 'passed arguments rIC',
+    test: function () {
+      var
+        executed = [],
+        first = function () {
+          executed.push(arguments);
+        },
+        second = function () {
+          executed.push(arguments);
+        }
+      ;
+      sob.idle(first, 1, 2, 3);
+      sob.idle(second, 4, 5);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          [].join.call(executed[0]) === '1,2,3' &&
+          [].join.call(executed[1]) === '4,5'
+        );
+      }), 300);
+    }
+  }, {
+    name: 'no duplicated rAF + args',
+    test: function () {
+      var
+        executed = [],
+        first = function first() {
+          executed.push({
+            fn: first,
+            ar: [].slice.call(arguments)
+          });
+        },
+        second = function second() {
+          executed.push({
+            fn: second,
+            ar: [].slice.call(arguments)
+          });
+        }
+      ;
+      sob.frame(first, 1, 2, 3);
+      sob.frame(first, 1, 2, 3);
+      sob.frame(second, 4, 5);
+      sob.frame(first, 1, 2, 3);
+      sob.frame(second, 4, 5);
+      sob.frame(second, 4, 5);
+      sob.frame(first, 1, 2, 3);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          executed.length === 2 &&
+          executed[0].fn === first &&
+          executed[1].fn === second &&
+          executed[0].ar.join(',') === '1,2,3' &&
+          executed[1].ar.join(',') === '4,5'
+        );
+      }), 300);
+    }
+  }, {
+    name: 'no duplicated rIC + args',
+    test: function () {
+      var
+        executed = [],
+        first = function first() {
+          executed.push({
+            fn: first,
+            ar: [].slice.call(arguments)
+          });
+        },
+        second = function second() {
+          executed.push({
+            fn: second,
+            ar: [].slice.call(arguments)
+          });
+        }
+      ;
+      sob.idle(first, 1, 2, 3);
+      sob.idle(first, 1, 2, 3);
+      sob.idle(second, 4, 5);
+      sob.idle(first, 1, 2, 3);
+      sob.idle(second, 4, 5);
+      sob.idle(second, 4, 5);
+      sob.idle(first, 1, 2, 3);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          executed.length === 2 &&
+          executed[0].fn === first &&
+          executed[1].fn === second &&
+          executed[0].ar.join(',') === '1,2,3' &&
+          executed[1].ar.join(',') === '4,5'
+        );
+      }), 300);
+    }
+  }, {
+    name: 'same callback, different rAF args',
+    test: function () {
+      var
+        executed = [],
+        first = function () {
+          executed.push({
+            fn: first,
+            ar: [].slice.call(arguments)
+          });
+        },
+        second = function () {
+          executed.push({
+            fn: second,
+            ar: [].slice.call(arguments)
+          });
+        },
+        ids = [
+          sob.frame(first),
+          sob.frame(first, 1),
+          sob.frame(first, 1, 2),
+          sob.frame(second),
+          sob.frame(second, 1),
+          sob.frame(first),
+          sob.frame(first, 1),
+          sob.frame(first, 1, 2),
+          sob.frame(second),
+          sob.frame(second, 1)
+        ]
+      ;
+      sob.clear(ids[1]);
+      sob.clear(ids[3]);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          executed.length === 3 &&
+          executed[0].fn === first &&
+          executed[1].fn === first &&
+          executed[2].fn === second &&
+          executed[0].ar.join(',') === '' &&
+          executed[1].ar.join(',') === '1,2' &&
+          executed[2].ar.join(',') === '1'
+        );
+      }), 300);
+    }
+  }, {
+    name: 'same callback, different rIC args',
+    test: function () {
+      var
+        executed = [],
+        first = function () {
+          executed.push({
+            fn: first,
+            ar: [].slice.call(arguments)
+          });
+        },
+        second = function () {
+          executed.push({
+            fn: second,
+            ar: [].slice.call(arguments)
+          });
+        },
+        ids = [
+          sob.idle(first),
+          sob.idle(first, 1),
+          sob.idle(first, 1, 2),
+          sob.idle(second),
+          sob.idle(second, 1),
+          sob.idle(first),
+          sob.idle(first, 1),
+          sob.idle(first, 1, 2),
+          sob.idle(second),
+          sob.idle(second, 1)
+        ]
+      ;
+      sob.clear(ids[1]);
+      sob.clear(ids[3]);
+      setTimeout(wru.async(function () {
+        wru.assert(
+          executed.length === 3 &&
+          executed[0].fn === first &&
+          executed[1].fn === first &&
+          executed[2].fn === second &&
+          executed[0].ar.join(',') === '' &&
+          executed[1].ar.join(',') === '1,2' &&
+          executed[2].ar.join(',') === '1'
+        );
+      }), 300);
+    }
+  }, {
+    name: 'overload',
+    test: function () {
+      sob.frame(function () {
+        var t = (new Date).getTime();
+        while ((new Date).getTime() - t < (1000 / sob.minFPS));
+      });
+      sob.frame(wru.async(function () {
+        wru.assert(sob.isOverloaded);
+      }));
     }
   }
 ]);
